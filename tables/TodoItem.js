fa2683b4555396bdc4fe5094a8a110ff91a34ead
danderson00 <danderson00@gmail.com>
2015-11-24 21:53:56 +0000

    Updated todoitem table for push notification

--- tables/TodoItem.js
+++ tables/TodoItem.js
@@ -6,23 +6,43 @@
 ** Sample Table Definition - this supports the Azure Mobile Apps
 ** TodoItem product with authentication and offline sync
 */
-var azureMobileApps = require('azure-mobile-apps');
+var azureMobileApps = require('azure-mobile-apps'),
+	logger = require('azure-mobile-apps/src/logger');
 
 // Create a new table definition
 var table = azureMobileApps.table();
 
-// Configure specific code when the client does a request
 // READ - only return records belonging to the authenticated user
-// table.read(function (context) {
-//   context.query.where({ userId: context.user.id });
-//   return context.execute();
-// });
+table.read(function (context) {
+  context.query.where({ userId: context.user.id });
+  return context.execute();
+});
 
 // CREATE - add or overwrite the userId based on the authenticated user
-// table.insert(function (context) {
-//   context.item.userId = context.user.id;
-//   return context.execute();
-// });
+table.insert(function (context) {
+	context.item.userId = context.user.id;
+
+    var payload = '<toast><visual><binding template="Toast01"><text id="1">Inserted item</text></binding></visual></toast>';
+    
+    return context.execute()
+        .then(function (results) {
+            if (context.push) {
+                context.push.wns.send(null, payload, 'wns/toast', function (error) {
+                    if (error) {
+                        logger.error('Error while sending push notification: ', error);
+                    } else {
+                        logger.silly('Push notification sent successfully!');
+                    }
+                });
+            }
+			// we want to return the results immediately, the result of the push 
+			// notification does not affect the result of the REST API call
+            return results;
+        });
+        // .catch(function (error) {
+        //     logger.error('Error while running context.execute: ', error);
+        // });
+});
 
 // UPDATE - for this scenario, we don't need to do anything - this is
 // the default version
@@ -32,9 +52,9 @@ var table = azureMobileApps.table();
 
 // DELETE - for this scenario, we don't need to do anything - this is
 // the default version
-//table.delete(function (context) {
-//  return context.execute();
-//});
+// table.delete(function (context) {
+//   return context.execute();
+// });
 
 // Finally, export the table to the Azure Mobile Apps SDK - it can be
 // read using the azureMobileApps.tables.import(path) method
